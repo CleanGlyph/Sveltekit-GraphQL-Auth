c991d9b0dd4978d1eb259902eece8aab16fd3fed
Lizz <68482497+CleanGlyph@users.noreply.github.com>
2021-10-25 11:45:37 -0400

    Update svelte.config.js
    Possible fix? https://github.com/timhall/svelte-apollo/issues/97

--- svelte.config.js
+++ svelte.config.js
@@ -7,18 +7,21 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
+    vite: {  
+      optimizeDeps: {
+        include: [
+          '@apollo/client/core',
+          '@apollo/client/cache',
+          '@apollo/client/link/ws',
+          '@apollo/client/link/context',
+          '@apollo/client/link/error',
+          '@apollo/client/utilities'
+        ],
+        exclude: ['@apollo/client']
+    },
+  }
   },
-  optimizeDeps: {
-    include: [
-      '@apollo/client/core',
-      '@apollo/client/cache',
-      '@apollo/client/link/ws',
-      '@apollo/client/link/context',
-      '@apollo/client/link/error',
-      '@apollo/client/utilities'
-    ],
-    exclude: ['@apollo/client']
-  },
+
   preprocess: [preprocess({
     postcss: true
   })]
